d49652084fd79aff7d1b9d855c9226fb373e7412
boryl <bjornolle.rylander@gmail.com>
2019-08-22 12:05:31 +0200

    heroku test

--- config.py
+++ config.py
@@ -9,15 +9,16 @@ load_dotenv(find_dotenv())
 class Config:
 	"""Base config vars."""
 	SECRET_KEY = os.getenv('SECRET_KEY')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	
     
 class ProdConfig(Config):
 	DEBUG = False
 	TESTING = False
 	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+	
 
 class DevConfig(Config):
 	DEBUG = True
 	TESTING = True
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sqlite3')
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
